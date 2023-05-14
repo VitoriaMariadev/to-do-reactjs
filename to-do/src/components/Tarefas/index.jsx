fccddfd4355bf6c3a42cd4e3132037bd13280bf1
VihTech <vitoriamaria.rodrigues02@gmail.com>
2023-05-14 18:46:18 -0300

    adicionando tarefas

--- to-do/src/components/Tarefas/index.jsx
+++ to-do/src/components/Tarefas/index.jsx
@@ -2,16 +2,11 @@ import { TarefasLista } from '../TarefasListas'
 import { useState, useEffect } from 'react'
 import './style.css'
 import { api } from "../../Services/api"
+import {BsTrash3} from 'react-icons/bs'
+import {GrCheckbox} from 'react-icons/gr'
+import {GrCheckboxSelected} from 'react-icons/gr'
 
-const Tarefas = () =>{
-
-    const [addTarefa, setAddTarefa] = useState([])
-
-    const add = () =>{
-        return(
-            setAddTarefa([<TarefasLista/>].concat(addTarefa))
-        )
-    }
+export const Tarefas = () =>{
 
     const [dia, setDia] = useState('Seg')
 
@@ -79,6 +74,8 @@ const Tarefas = () =>{
     }
 
     const addToDo = async () => {
+        console.log(tarefas)
+        
         const data = {
             nome,
             dia,
@@ -96,6 +93,28 @@ const Tarefas = () =>{
         }
 
     }
+
+    const [addTarefa, setAddTarefa] = useState([])
+
+    const add = () =>{
+        return(
+            setAddTarefa([<TarefasLista addFuncao={addToDo} set={setAdicionarTodo}/>].concat(addTarefa))
+        )
+    }
+
+    const [checkBox, setCheckBox] = useState(<GrCheckbox/>)
+    const [completo, setCompleto] = useState(false)
+
+    const check = () => {
+        if (completo === false){
+            setCheckBox(<GrCheckboxSelected/>)
+            setCompleto(true)
+        }
+        else{
+            setCheckBox(<GrCheckbox/>)
+            setCompleto(false)
+        }
+        }
 
     return(
         <main className='main-tarefas'>
@@ -116,20 +135,18 @@ const Tarefas = () =>{
                 </div>
 
                 <div className="tarefas" id="tarefas">
-                    {addTarefa}
+                    <TarefasLista addFuncao={addToDo} set={setAdicionarTodo}/>
 
                     {
                         carregando === true ?(
                             <>
-                            
                             {pegarLista.map((items, index) => (                            
                                 <div className="espacos" key={index}>
                                     <div className="checar">
-                                        <input type="checkbox" className="tarefas-checar"/>
+                                        <div onClick={check} className='check'>{checkBox}</div>
                                     </div>
-                                        <input type="text" className="tarefas-input" placeholder={items.tarefas} onChange={(e) => setAdicionarTodo(e.target.value)}></input>
-                                        <button onClick={addToDo}>Add</button>
-                                        <button onClick={() => {deletar(items._id)}}>Del</button>
+                                        <input type="text" className="tarefas-input" value={items.tarefas}></input>
+                                        <button className='btndel' onClick={() => {deletar(items._id)}}><BsTrash3/></button>
                                 </div>
                             ))}
 
--- to-do/src/components/TarefasListas/index.jsx
+++ to-do/src/components/TarefasListas/index.jsx
@@ -1,14 +1,36 @@
 import './style.css'
+import {TiPlus} from 'react-icons/ti'
+import {GrCheckbox} from 'react-icons/gr'
+import {GrCheckboxSelected} from 'react-icons/gr'
+import { useState } from 'react'
 
-export const TarefasLista = () =>{
+
+export const TarefasLista = (props) =>{
+
+    const [checkBox, setCheckBox] = useState(<GrCheckbox/>)
+    const [completo, setCompleto] = useState(false)
+
+    const set = props.set
+
+    const check = () => {
+        if (completo === false){
+            setCheckBox(<GrCheckboxSelected/>)
+            setCompleto(true)
+        }
+        else{
+            setCheckBox(<GrCheckbox/>)
+            setCompleto(false)
+        }
+
+    }
+
     return(
         <div className="espacos">
             <div className="checar">
-                <input type="checkbox" className="tarefas-checar"/>
+                <div onClick={check} className='check'>{checkBox}</div>
             </div>
-            <input type="text" className="tarefas-input"/>
-            <button>Add</button>
-            <button>Del</button>
+            <input type="text" className="tarefas-input" onChange={(e) => set(e.target.value)}/>
+            <button className='btnadd' onClick={props.addFuncao}><TiPlus/></button>
         </div>
     )
 }
